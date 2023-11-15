c2e51f3510bb2ae96819e30bde4c60be5d9d73bc
LeadDev <neiltiar@hotmail.com>
2023-11-15 10:38:14 +0100

    initialisation

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+     
+      <main>
+        <h1>Header</h1>
+        
+      </main>
+    
+    </div>
+  </template>
+  
+  <script>
+ 
+  export default {
+    name: 'component-header'
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// src/main.js est l'instance de l'application Vue.
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+
