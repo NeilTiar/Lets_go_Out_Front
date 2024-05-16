5553a9901349432e498062f3ead59b3f7b9513ea
LeadDev <neiltiar@hotmail.com>
2024-05-16 11:54:34 +0200

    feature
    add input to load main picture and display it specific div

--- src/views/Create-review.vue
+++ src/views/Create-review.vue
@@ -3,43 +3,76 @@
 
    
     <h2 class="title">Créer votre nouvelle review :</h2>
+
     <div class="container-create-review-form">
-        <div class="select-theme">
-            <select 
-                v-model="selectedTheme"
-                class="theme-select"
-            >
-                <option
-                    disabled
-                    selected
-                    value=""
-                    hidden
+        <form
+            class="create-form"
+            @submit.prevent="submitForm"
+        >
+            <div class="select-theme">
+                <select 
+                    v-model="formData.selectedTheme"
+                    class="theme-select"
                 >
-                    Sélectionnez un theme
-                </option>
-                <option value="Food&Drinks">Food&drinks</option>
-                <option value="Loisirs">Loisirs</option>
-                <option value="Culture">Culture</option>
-            </select>
-        </div>
-        <input
-            class="input-place"
-            type="text" 
-            placeholder="Nom du lieux ou de l'établissement"
-        >
+                    <option
+                        disabled
+                        selected
+                        value=""
+                        hidden
+                    >
+                        Sélectionnez un theme
+                    </option>
+                    <option value="Food&Drinks">Food&drinks</option>
+                    <option value="Loisirs">Loisirs</option>
+                    <option value="Culture">Culture</option>
+                </select>
+            </div>
 
-        <input
-            class="input-address"
-            type="text" 
-            placeholder="Adresse du lieux ou de l'établissement"
-        >
+            <input
+                v-model="formData.placeName"
+                class="input-place"
+                type="text" 
+                placeholder="Nom du lieux ou de l'établissement"
+            >
 
-        <input
-            class="input-district"
-            type="number" 
-            placeholder="numero de l'arrondissement"
+            <input
+                v-model="formData.address"
+                class="input-address"
+                type="text" 
+                placeholder="Adresse du lieux ou de l'établissement"
+            >
+
+            <input
+                v-model="formData.district"
+                class="input-district"
+                type="number" 
+                placeholder="numero de l'arrondissement"
+            >
+
+            <input
+                class="input-main-picture"
+                type="file" 
+                @change="onFileLoad"
+            >
+        </form>
+    </div>  
+
+    <div
+        class="displayIMG"
+    >
+        <img
+            :src="imageURL"
+            alt="uploaded"
         >
     </div>
+
+    <button
+        class="submit-form"
+        @click="submitForm"
+    >
+        Enregistrer votre Review
+    </button> 
+  
 
     <FooterComponent />
 </template>
@@ -67,9 +100,21 @@ export default {
   data() {
 
     return {
-    
+
+    formData: {
+ 
+     selectedTheme: "",
+     placeName: "",
+     district: "",
+     address: "",
+     mainPicture:"",
+
+
+    },
+
     isDesktop: window.innerWidth > 768,
     selectedTheme: "",
+    imageURL:"",
 
     }
   },
@@ -94,8 +139,36 @@ export default {
 
   methods: {
 
+    onFileLoad(event){
+    
+    const file = event.target.files[0];
+    this.imageURL = URL.createObjectURL(file);
 
 
+    },
+
+
+ submitForm() {
+      // Effectuez ici toute action que vous souhaitez effectuer lorsque le formulaire est soumis
+      // Vous pouvez accéder aux données du formulaire via this.formData
+      console.log('Données du formulaire :', this.formData);
+      
+      // Réinitialiser les données du formulaire après la soumission si nécessaire
+     // Réinitialiser toutes les propriétés du formulaire, object assign permet de reinitialiser en un seul temps 
+     /*   
+     c'est l'equivalent des lignes ci dessous :
+          this.formData.district = '';
+          this.formData.address = '';
+          this.formData.placeName = '';  etc.....*/
+
+
+  Object.assign(this.formData, {
+    district: '',
+    address: '',
+    placeName: '',
+    selectedTheme: '',
+  });
+    }
 
 
 
@@ -182,5 +255,24 @@ body {
   border-radius: 25px;
   margin-top: 0.5rem;
   font-size: 1rem;
+}
+
+.create-form {
+
+  display: flex;
+  flex-direction: column;
+}
+
+.displayIMG {
+
+  width: 25%;
+  height:14rem;
+  background-color: aqua;
+}
+
+img {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
 }
 </style>
